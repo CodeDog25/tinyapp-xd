cbd535373a645401d3a9ee841928e534bfc62a8f
Sicily Ding <xishading@gmail.com>
2023-01-13 13:20:20 -0700

    redirect short urls

--- express_server.js
+++ express_server.js
@@ -4,7 +4,11 @@ const PORT = 8080; // default port 8080
 
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
 
+const generateRandomString = () => {
+    return Math.random().toString(36).substring(2, 8)
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +32,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
@@ -36,9 +44,17 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`urls/${id}`);
+});
 
-
-
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
 
 
