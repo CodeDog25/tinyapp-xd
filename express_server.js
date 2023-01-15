88af8547a0333529709b3b6cbd9e18d67caba132
Sicily Ding <xishading@gmail.com>
2023-01-14 22:42:48 -0700

    pass username

--- express_server.js
+++ express_server.js
@@ -44,7 +44,10 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+};
   res.render("urls_index", templateVars);
 });
 
@@ -54,6 +57,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { 
+    username: req.cookies["username"],
     id: req.params.id, 
     longURL: urlDatabase[req.params.id]
 };
