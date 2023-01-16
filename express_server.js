2e93ad73c4def397287d8d7d6331d06c7d24573d
Sicily Ding <xishading@gmail.com>
2023-01-15 23:06:58 -0700

    add security features

--- express_server.js
+++ express_server.js
@@ -3,15 +3,20 @@ const morgan = require("morgan");
 const app = express();
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 
 /////////////////////////////////////////
 //////////MIDDLEWARES
 /////////////////////////////////////////
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
+app.use(
+    cookieSession({
+      name: "session",
+      keys: ["dingding"]
+}));
 
 
 //Functions
@@ -20,14 +25,23 @@ const generateRandomString = () => {
 };
 
 const getUserByEmail = (email) => {
-  for (const uid in usersDatabse) {
-    if (usersDatabse[uid].email === email) {
-        return usersDatabse[uid].id;
+  for (const uid in usersDatabase) {
+    if (usersDatabase[uid].email === email) {
+        return usersDatabase[uid].id;
     }
   }
   return null;
 };
 
+const urlsForUser = (urlDatabase, id) => {
+    let userURL = {};
+    for (const key in urlDatabase) {
+      if (urlDatabase[key].userID === id) {
+        userURL[key] = urlDatabase[key];
+    }
+  }   
+  return userURL;
+};
 /////////////////////////////////////////
 //////////DATABASE
 /////////////////////////////////////////
@@ -43,17 +57,17 @@ const urlDatabase = {
     },
 };
 
-const usersDatabse = {
-    userRandomID: {
-      id: "1snymq",
+const usersDatabase = {
+    sn1ymq: {
+      id: "sn1ymq",
       email: "user@example.com",
       password: "purple-monkey-dinosaur",
     },
-    user2RandomID: {
-      id: "4upxwu",
+    up4xwu: {
+      id: "up4xwu",
       email: "test@lhl.com",
       password: "lhl",
-    }
+    },
 };
 
 /////////////////////////////////////////
@@ -78,18 +92,23 @@ app.get("/hello", (req, res) => {
 
 // GET /urls
 app.get("/urls", (req, res) => {
-    const templateVars = { 
-      urls: urlDatabase,
-      user: usersDatabse[req.cookies["user_id"]]
-  };
+if (usersDatabase[req.session.user_id]) {
+    const templateVars = {
+      user: usersDatabase[req.session.user_id],
+      urls: urlsForUser(urlDatabase, req.session.user_id)
+    };
+
     res.render("urls_index", templateVars);
+  } else {
+    res.status(401).send("Please Login before continue!");
+  }
 });
 
 
 // GET /urls/new
 app.get("/urls/new", (req, res) => {
     const templateVars = {
-      user: usersDatabse[req.cookies["user_id"]]
+      user: usersDatabase[req.session.user_id]
     };
     if (!templateVars.user) {
         res.redirect("/login");
@@ -97,46 +116,52 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new", templateVars);
 });
   
-// GET /urls/:id
+// GET /urls/:id  **
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { 
-      user: usersDatabse[req.cookies["user_id"]],
-      id: id, 
-      longURL: urlDatabase[id].longURL
-  };
-    if (!usersDatabse[templateVars.id]) {
-    res.send("This id does not exist!");
-  }
-    res.render("urls_show", templateVars);
+    const newURL = req.body.longURL;
+    const urlID = req.params.id;
+    let userID = usersDatabase[req.session.user_id];
+  
+    if (!urlDatabase[urlID]) {
+      return res.status(400).send("The URL does not exist!");
+    }
+    if (!userID) {
+      return res.status(401).send("Please Login");
+    }
+ 
+    if (urlDatabase[urlID].userID !== userID) {
+      return res.status(401).send("You don't own the URL");
+    }
+    urlDatabase[urlID].longURL = newURL;
+    res.redirect('/urls');
 });
 
-
-// GET /u/:id
+// GET /u/:id   
 app.get("/u/:id", (req, res) => {
     if (urlDatabase[req.params.id]) {
-    const longURL = urlDatabase[req.params.id].longURL;
-    res.redirect(longURL);
-   } 
-   res.send("this id doesn't exist");
+      res.redirect(urlDatabase[req.params.id].longURL);
+   } else {
+     res.status(404).send("This url does not exist!");
+   }
+   
 });
 
 
 // GET /register
 app.get("/register", (req, res) => {
-  const templateVars = { 
-    user: usersDatabse[req.cookies["user_id"]] 
-  };
-  if (templateVars.user) {
-    res.redirect("/urls");
+if (!usersDatabase[req.session.user_id]) {
+    const templateVars = {
+      user: usersDatabase[req.session.user_id],
+    };
+    res.render("urls_register", templateVars);
   }
-  res.render("urls_register", templateVars);
+  res.redirect("/urls");
 });
-
 
 // GET /login
 app.get("/login", (req, res) => {
     const templateVars = {
-      user: usersDatabse[req.cookies["user_id"]]
+      user: usersDatabase[req.session.user_id]
     };
     if (templateVars.user) {
       res.redirect("/urls");
@@ -147,26 +172,27 @@ app.get("/login", (req, res) => {
 
 // POST /urls
 app.post("/urls", (req, res) => {
-  let currentUser = usersDatabse[req.cookies["user_id"]];
-  if (!currentUser) {
-    res.redirect("Please login");
-  }
-
-  const id = generateRandomString();
-  const longURL = req.body.longURL;
-  urlDatabase[id] = {
-    longURL: longURL,
-    userID: req.cookies["user_id"]
+  const userID = req.session.user_id;
+  const userURL = urlsForUser(urlDatabase, userID);
+  const templateVars = {
+    urls: userURL,
+    user: usersDatabase[userID]
   };
-  res.redirect(`urls/${id}`);
+
+  if (!templateVars.user) {
+    return res.status(401).send("Please login to view!");
+  }  
+   res.render('urls_index', templateVars);
 });
 
 
-// POST /urls/:id/delete
+
+
+// POST /urls/:id/delete **
 app.post("/urls/:id/delete", (req, res) => {
-  let currentUser = usersDatabse[req.cookies["user_id"]];
+  let currentUser = usersDatabase[req.session.user_id];
   if (currentUser) {
-    delete urlDatabase[req.params.id].longURL;
+    delete urlDatabase[req.params.id];
     res.redirect("/urls");
   } else {
     res.send("You cannot delete it!");
@@ -175,15 +201,21 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // POST /urls/:id
 app.post("/urls/:id", (req,res) => {
-  if (usersDatabse[req.cookies["user_id"]]) {
-    const id = req.params.id;  
-    const longURL = req.body.longURL;
+const id = req.params.id;
+const longURL = req.body.longURL;
+  if (
+    users[req.session.user_id] &&
+    req.session.user_id === urlDatabase[id].userID
+)   {
     urlDatabase[id].longURL = longURL;
-    res.redirect(`/urls/${id}`);
-  } else {
-    res.send("Cannot access!");
+     res.redirect("/urls");
+ } else {
+  res.status(401).send("You cannot Access the page");
 }
 });
+
+
+
 
 // POST /login
 app.post("/login", (req, res) => {
@@ -192,38 +224,40 @@ app.post("/login", (req, res) => {
   if (user === null) {
     return res.status(403).send("Email does not exist!");
   }
-  if (!user || user.password !== password) {
+  if (!user || !bcrypt.compareSync(password, user.hashedPass)) {
     return res.status(403).send("Incorrect email and/or password!");
   }
-  res.cookie("user_id", user.id); 
+  res.session.user_id = user.id; 
   res.redirect(`/urls`);
 });
 
+
 // POST /logout
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  res.clearCookie("session");
   res.redirect("/login");
-});
+}); 
+
 
 // POST /register
 app.post("/register", (req,res) => {
     const { email, password} = req.body;
-  
+    const hashedPass = bcrypt.hashSync(password, salt);
     if (!email || !password) {
       return res.status(400).send("Missing email and/or password!");
     }
   
-    if (getUserByEmail(email)) {
+    if (getUserByEmail(email, usersDatabase)) {
       return res.status(400).send("This email has already exist!");
     }
     
     const id = generateRandomString();
-    usersDatabse[id] = {
+    usersDatabase[id] = {
         id,
         email,
-        password
+        hashedPass
     };
-    res.cookie("user_id", id);
+    req.session.user_id = id;
     res.redirect("/urls");
   });
 
